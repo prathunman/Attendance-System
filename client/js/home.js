0d13f52900b03d49000671f3c0e84db7e05a0bb8
prathunman <prathiunmansathiabalan@gmail.com>
2025-02-02 17:36:55 +0530

    #4.3, Redirect to the home page on logout

---
+++ client/js/home.js
@@ -0,0 +1,27 @@
+document.getElementById('profileImage').addEventListener('click', function() {
+    let dropdown = document.getElementById('dropdownMenu');
+    if (dropdown.style.display === 'block') {
+      dropdown.style.display = 'none';
+    } else {
+      dropdown.style.display = 'block';
+    }
+  });
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('#profileImage')) {
+      let dropdown = document.getElementById('dropdownMenu');
+      if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+      }
+    }
+};
+
+document.getElementById('logout').addEventListener('click', function(event) {
+    event.preventDefault();
+    logout();
+});
+  
+function logout() {
+    sessionStorage.removeItem('jwtToken'); 
+    window.location.href = '/login.html'; 
+}
